e8288b07d42388ffdb693dbb666c12050fac463f
Kazuuma-19 <kazu15870@gmail.com>
2025-04-17 17:59:08 +0900

    feat: biome setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,36 @@
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
 import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
 
 type Todo = {
-  id: string;
-  completed: boolean;
-  name: string;
-  date: string;
+	id: string;
+	completed: boolean;
+	name: string;
+	date: string;
 };
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+	const [todos, setTodos] = useState<Todo[]>([]);
 
-  const getTodos = async () => {
-    const response = await axios.get("http://localhost:8080/todos");
-    setTodos(response.data);
-  };
+	const getTodos = useCallback(async () => {
+		const response = await axios.get("http://localhost:8080/todos");
+		setTodos(response.data);
+	}, []);
 
-  useEffect(() => {
-    getTodos();
-  }, []);
+	useEffect(() => {
+		getTodos();
+	}, [getTodos]);
 
-  return (
-    <>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          <h2>{todo.name}</h2>
-          <p>{todo.date}</p>
-        </div>
-      ))}
-      <Button>Click me</Button>
-    </>
-  );
+	return (
+		<>
+			{todos.map((todo) => (
+				<div key={todo.id}>
+					<h2>{todo.name}</h2>
+					<p>{todo.date}</p>
+				</div>
+			))}
+			<Button>Click me</Button>
+		</>
+	);
 }
 export default App;
