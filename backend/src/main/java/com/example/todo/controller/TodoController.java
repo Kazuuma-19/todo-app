0b7483e25a27a1724cce5bc20ba642fc4c7a7d2d
Kazuuma-19 <kazu15870@gmail.com>
2025-04-17 15:21:03 +0900

    feat: send a request and receive in the spring

--- backend/src/main/java/com/example/todo/controller/TodoController.java
+++ backend/src/main/java/com/example/todo/controller/TodoController.java
@@ -3,10 +3,12 @@ package com.example.todo.controller;
 import com.example.todo.model.Todo;
 import com.example.todo.repository.TodoRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequestMapping("/todos")
 @RestController
 public class TodoController {
     private final TodoRepository todoRepository;
@@ -15,7 +17,7 @@ public class TodoController {
         this.todoRepository = todoRepository;
     }
 
-    @GetMapping("/todo")
+    @GetMapping
     public List<Todo> getTodos() {
         return todoRepository.findAll();
     }
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,36 @@
 import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+type Todo = {
+  id: string;
+  completed: boolean;
+  name: string;
+  dateTime: Date;
+};
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const getTodos = async () => {
+    const response = await axios.get("http://localhost:8080/todos");
+    setTodos(response.data);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <>
-      <h1>Hello World!</h1>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <h2>{todo.name}</h2>
+          {/* <p>{todo.dateTime}</p> */}
+        </div>
+      ))}
       <Button>Click me</Button>
     </>
   );
 }
-
 export default App;
