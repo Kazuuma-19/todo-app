fdd1fd39cb1c98727afacca7db5fdb2d2fa2cd64
Kazuma <kazu15870@gmail.com>
2025-05-05 17:35:59 +0900

    chore: fix the way of writing generate token method

--- backend/src/main/java/com/example/todo/service/JwtService.java
+++ backend/src/main/java/com/example/todo/service/JwtService.java
@@ -1,31 +1,29 @@
 package com.example.todo.service;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class JwtService {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    public SecretKey getSecretKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
     public String generateToken(String email) {
-        SecretKey key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
-
-        Map<String, Object> claims = new HashMap<>();
-        claims.put("sub", email);
-        claims.put("iat", new Date()); // 発行時間
-
+        SecretKey key = getSecretKey();
         return Jwts.builder()
-                .claims(claims)
-                .expiration(new Date(System.currentTimeMillis() + 3600_000)) // 1時間有効
+                .subject(email)
+                .issuedAt(new Date())
                 .signWith(key)
                 .compact();
     }
