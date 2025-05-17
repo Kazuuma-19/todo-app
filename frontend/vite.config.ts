85b38de5127e47e7aebbcb6229f78200b11c385a
Kazuma <kazu15870@gmail.com>
2025-05-17 13:59:10 +0900

    feat: install vitest and  react testing library

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
@@ -15,5 +16,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    globals: true, //　test, expectなどのimportが不要に
+    environment: "happy-dom",
+    setupFiles: "./src/testing/setup.ts",
   },
 });
