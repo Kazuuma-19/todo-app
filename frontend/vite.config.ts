055d0ca9b57757be10d54018216af1951368d0fa
Kazuma <kazu15870@gmail.com>
2025-06-01 21:43:38 +0900

    feat: setting sentry for frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,33 +2,46 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    build: {
+      sourcemap: true,
     },
-  },
-  test: {
-    globals: true, //　test, expectなどのimportが不要に
-    environment: "happy-dom",
-    setupFiles: "./src/testing/setup.ts",
-    env: {
-      VITE_BACKEND_API_URL: "http://localhost:8080", // テスト用の環境変数
+    plugins: [
+      TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+      react(),
+      tailwindcss(),
+      sentryVitePlugin({
+        org: "kazuuma",
+        project: "todo-app-frontend",
+        authToken: env.SENTRY_AUTH_TOKEN,
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-    coverage: {
-      // you can include other reporters, but 'json-summary' is required, json is recommended
-      reporter: ["text", "json-summary", "json", "lcov"],
-      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
-      reportOnFailure: true,
+    test: {
+      globals: true, //　test, expectなどのimportが不要に
+      environment: "happy-dom",
+      setupFiles: "./src/testing/setup.ts",
+      env: {
+        VITE_BACKEND_API_URL: "http://localhost:8080", // テスト用の環境変数
+      },
+      coverage: {
+        // you can include other reporters, but 'json-summary' is required, json is recommended
+        reporter: ["text", "json-summary", "json", "lcov"],
+        // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+        reportOnFailure: true,
+      },
     },
-  },
+  };
 });
